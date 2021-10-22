1e39b7f4e7769366d5f8d5735ee94ab7c2814134
mock1ngj <82967608+mock1ngj@users.noreply.github.com>
2021-10-22 21:42:45 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -2,77 +2,92 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 
-static void fifoAlgo(GtkWidget *widget, gpointer data)
+typedef struct
 {
+    int i;
+}data;
 
+typedef struct
+{
+    GtkWidget *window;
+    GtkWidget *label;
+    GtkWidget *grid;
+    GtkWidget *entry;
+    
+}Widget;
+
+void value_changed(GtkRange *range, gpointer win) 
+{
+    
+   gdouble val = gtk_range_get_value(range);
+   gchar *str = g_strdup_printf("%.f", val);    
+   gtk_label_set_text(GTK_LABEL(win), str);
+   
+   g_free(str);
+}
+static void fifoAlgo(GtkWidget *widget, gpointer in)
+{
+    data *input = in;
+    printf("%d\n", input->i);
+    input->i++;
+}
+
+static void button_click(GtkWidget *widget, gpointer user_data)
+{
+    int in;
+    in = atoi(gtk_entry_get_text(GTK_ENTRY(user_data)));
+    printf("%d\n", in);
 }
 
 static void button_clicked(GtkWidget *widget, gpointer data)
 {
-    int memSize[5] = {300, 200, 100, 500, 780};
-    int input[5], j;
-    
-    input[j] = atoi(gtk_entry_get_text(GTK_ENTRY(data)));
-
-    if ((memSize[j] - input[j]) >= 0)
-    {
-        printf("Allocated at block %d with %d of internal fragmentation\n", j, memSize[j]-input[j]);
-    }
-    j++;
+    Widget* input=(Widget*) data;
+    g_print("%s\n", gtk_entry_get_text(GTK_ENTRY(input->entry)));
 }
 
 int main(int argc, char* argv[])
 {
     gtk_init(&argc, &argv);
-    GtkWidget *window, *menubox, *fifo, *bestfit;
-    GtkWidget *menuBar, *grid, *entry, *box;
-
+    GtkWidget *fifo, *bestfit;
+    Widget widget;
+    data user_data;
+    user_data.i = 0;
 	//creates window starts here
 
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(window, "Scheduling Algorithm");
+    widget.window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(widget.window, "Scheduling Algorithm");
     //sets the size of window by 500x500
-    gtk_window_set_default_size(window, 700, 700);
+    gtk_window_set_default_size(widget.window, 700, 700);
     //sets the position on the center of the screen
-    gtk_window_set_position(window, GTK_WIN_POS_CENTER_ALWAYS);
+    gtk_window_set_position(widget.window, GTK_WIN_POS_CENTER_ALWAYS);
 	//exit function for the application
-    g_signal_connect(window, "delete-event", G_CALLBACK(gtk_main_quit), NULL);
+    g_signal_connect(widget.window, "delete-event", G_CALLBACK(gtk_main_quit), NULL);
 	
     //ends here
 
 
-    grid = gtk_grid_new();
-    gtk_container_add(GTK_CONTAINER(window), grid);
+    widget.grid = gtk_grid_new();
+    gtk_container_add(GTK_CONTAINER(widget.window), widget.grid);
 
-    entry = gtk_entry_new();
+    widget.entry = gtk_entry_new();
 
-    /*menu creation starts here
-    creates a box for the menu
-    */
-    menubox = gtk_vbox_new(FALSE, 0);
+    //creates the FIFO button
+    fifo = gtk_button_new_with_label("FIFO");
+    gtk_widget_set_size_request(fifo, 70, 30);
+    gtk_grid_attach(GTK_GRID(widget.grid), fifo, 0, 0, 1, 1);
+    //creates the BEST-FIT button
+    bestfit = gtk_button_new_with_label("BEST-FIT");
+    gtk_widget_set_size_request(bestfit, 70, 30);
+    gtk_grid_attach(GTK_GRID(widget.grid), bestfit, 1, 0, 1, 1);
 
-    //creates a new menuBar object
-    menuBar = gtk_menu_bar_new();
-    //creates a new menu object
+    g_signal_connect(G_OBJECT(fifo), "clicked", G_CALLBACK(fifoAlgo), &user_data);
+    g_signal_connect(G_OBJECT(bestfit), "clicked", G_CALLBACK(gtk_main_quit), NULL);
+    g_signal_connect(widget.entry, "activate", G_CALLBACK(button_clicked), &widget);
 
-    //creates the labels for the menu objeect
-    fifo = gtk_menu_item_new_with_label("FIFO");
-    bestfit = gtk_menu_item_new_with_label("BEST-FIT");
-
-    //assigns the label to the menuBar object
-    gtk_menu_shell_append(GTK_MENU_SHELL(menuBar), bestfit);
-    gtk_menu_shell_append(GTK_MENU_SHELL(menuBar), fifo);
-    gtk_box_pack_start(GTK_BOX(menubox), menuBar, FALSE, FALSE, 0);
-
-    g_signal_connect(G_OBJECT(fifo), "activate", G_CALLBACK(gtk_main_quit), NULL);
-    g_signal_connect(G_OBJECT(bestfit), "activate", G_CALLBACK(gtk_main_quit), NULL);
-    g_signal_connect(entry, "activate", G_CALLBACK(button_clicked), entry);
    
     //ends here
-    gtk_grid_attach(GTK_GRID(grid), menubox, 0, 0, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), entry, 0, 1, 1, 1);
 
-    gtk_widget_show_all(window);
+    gtk_widget_show_all(widget.window);
     gtk_main();
     return 0;
 }
