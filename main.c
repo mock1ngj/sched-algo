f384640210f44a081ffee29fb653509b4cf698fc
mock1ngj <82967608+mock1ngj@users.noreply.github.com>
2021-10-23 00:48:01 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -10,11 +10,26 @@ typedef struct
 typedef struct
 {
     GtkWidget *window;
-    GtkWidget *label;
+    GtkWidget *vlbl;
     GtkWidget *grid;
     GtkWidget *entry;
-    
-}Widget;
+    GtkWidget *fifobtn;
+    GtkWidget *bestfitbtn;
+    GtkWidget *vscale;
+    GtkWidget *vsclttl;
+}GUI;
+
+static void fifoAlgo(GtkWidget *widget, gpointer in)
+{
+    data *input = in;
+    printf("%d\n", input->i);
+    input->i++;
+}
+
+static void bestFitAlgo(GtkWidget* widget, gpointer in)
+{
+
+}
 
 void value_changed(GtkRange *range, gpointer win) 
 {
@@ -25,69 +40,66 @@ void value_changed(GtkRange *range, gpointer win)
    
    g_free(str);
 }
-static void fifoAlgo(GtkWidget *widget, gpointer in)
-{
-    data *input = in;
-    printf("%d\n", input->i);
-    input->i++;
-}
 
-static void button_click(GtkWidget *widget, gpointer user_data)
-{
-    int in;
-    in = atoi(gtk_entry_get_text(GTK_ENTRY(user_data)));
-    printf("%d\n", in);
-}
-
-static void button_clicked(GtkWidget *widget, gpointer data)
-{
-    Widget* input=(Widget*) data;
-    g_print("%s\n", gtk_entry_get_text(GTK_ENTRY(input->entry)));
-}
 
 int main(int argc, char* argv[])
 {
-    gtk_init(&argc, &argv);
-    GtkWidget *fifo, *bestfit;
-    Widget widget;
+    GtkWidget *vbox, *boxbtn;
+    GUI gui;
     data user_data;
     user_data.i = 0;
+
+    gtk_init(&argc, &argv);
 	//creates window starts here
 
-    widget.window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(widget.window, "Scheduling Algorithm");
+    gui.window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(gui.window, "Scheduling Algorithm");
     //sets the size of window by 500x500
-    gtk_window_set_default_size(widget.window, 700, 700);
+    gtk_window_set_default_size(gui.window, 700, 700);
     //sets the position on the center of the screen
-    gtk_window_set_position(widget.window, GTK_WIN_POS_CENTER_ALWAYS);
+    gtk_window_set_position(gui.window, GTK_WIN_POS_CENTER_ALWAYS);
 	//exit function for the application
-    g_signal_connect(widget.window, "delete-event", G_CALLBACK(gtk_main_quit), NULL);
+    g_signal_connect(gui.window, "delete-event", G_CALLBACK(gtk_main_quit), NULL);
 	
     //ends here
 
+    vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL , 10);
+    boxbtn = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
 
-    widget.grid = gtk_grid_new();
-    gtk_container_add(GTK_CONTAINER(widget.window), widget.grid);
-
-    widget.entry = gtk_entry_new();
-
+    //creates the slider GUI
+    gui.vscale = gtk_scale_new_with_range(GTK_ORIENTATION_VERTICAL,1, 10, 1);
+    gtk_scale_set_draw_value(GTK_SCALE(gui.vscale), FALSE);
+    gtk_widget_set_size_request(gui.vscale, -1, 150);
+    gui.vlbl = gtk_label_new("");
+    gui.vsclttl = gtk_label_new("Number of Processes");
+  
     //creates the FIFO button
-    fifo = gtk_button_new_with_label("FIFO");
-    gtk_widget_set_size_request(fifo, 70, 30);
-    gtk_grid_attach(GTK_GRID(widget.grid), fifo, 0, 0, 1, 1);
+    gui.fifobtn = gtk_button_new_with_label("FIFO");
     //creates the BEST-FIT button
-    bestfit = gtk_button_new_with_label("BEST-FIT");
-    gtk_widget_set_size_request(bestfit, 70, 30);
-    gtk_grid_attach(GTK_GRID(widget.grid), bestfit, 1, 0, 1, 1);
+    gui.bestfitbtn = gtk_button_new_with_label("BEST-FIT");
 
-    g_signal_connect(G_OBJECT(fifo), "clicked", G_CALLBACK(fifoAlgo), &user_data);
-    g_signal_connect(G_OBJECT(bestfit), "clicked", G_CALLBACK(gtk_main_quit), NULL);
-    g_signal_connect(widget.entry, "activate", G_CALLBACK(button_clicked), &widget);
+    //packs the button on boxbtn container
+    gtk_box_pack_start(GTK_BOX(boxbtn), gui.fifobtn,FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(boxbtn), gui.bestfitbtn, FALSE, FALSE, 0);
 
+    //packs all the objects in vbox container
+    gtk_box_pack_start(GTK_BOX(vbox), gui.vsclttl, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), gui.vscale, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), gui.vlbl, FALSE, FALSE, 0);
+
+    //attaches both the boxbtn and hbox container in the grid
+    gui.grid = gtk_grid_new();
+    gtk_container_add(GTK_CONTAINER(gui.window), gui.grid);
+    gtk_grid_attach(GTK_GRID(gui.grid), boxbtn, 0, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(gui.grid), vbox, 0, 1, 1, 1);
+    gui.entry = gtk_entry_new();
+
+    //handles the signals like mouse click and key presses
+    g_signal_connect(G_OBJECT(gui.fifobtn), "clicked", G_CALLBACK(fifoAlgo), &user_data);
+    g_signal_connect(G_OBJECT(gui.bestfitbtn), "clicked", G_CALLBACK(gtk_main_quit), NULL);
+    g_signal_connect(gui.vscale, "value-changed", G_CALLBACK(value_changed), gui.vlbl);      
    
-    //ends here
-
-    gtk_widget_show_all(widget.window);
+    gtk_widget_show_all(gui.window);
     gtk_main();
     return 0;
 }
